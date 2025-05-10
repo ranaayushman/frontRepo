d03db1a315b5a2c814fcf1addfa0b0b3e530ebdb
Ayushman Rana <ayushmanr621@gmail.com>
2025-05-10 11:15:47 +0530

    responsive footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,12 +3,16 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="border-t border-white/20 text-center text-md text-[#110072] flex items-center justify-center gap-1">
-      <Copyright size={14} />
-      <span className="text-[#110072]">2025 copyright</span>
-      <span className="text-[#110072]">namankan.in</span>
-      <span className="text-[#110072]">all rights reserved</span>
-    </div>
+    <footer className="border-t border-white/20 text-center text-sm md:text-md text-[#110072] py-4 px-2">
+      <div className="flex flex-col md:flex-row items-center justify-center gap-1 md:gap-2 flex-wrap">
+        <div className="flex items-center gap-1">
+          <Copyright size={14} />
+          <span className="text-[#110072]">2025</span>
+        </div>
+        <span className="text-[#110072]">namankan.in</span>
+        <span className="text-[#110072]">All rights reserved</span>
+      </div>
+    </footer>
   );
 };
 
